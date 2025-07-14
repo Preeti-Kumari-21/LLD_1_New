5b036c597ff40df802235dd3aceae5c0d40e583f
Preeti <preeti.kumari241116@gmail.com>
2025-07-14 12:02:12 +0530

    First commit:14th July 2025: Add Java OOPS intro project

---
+++ src/main/java/org/example/introToOOPS/Main.java
@@ -0,0 +1,17 @@
+package main.java.org.example.introToOOPS;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Create a Student object
+        Student student = new Student();
+
+        // Set properties
+        student.name = "Preeti Kumari";
+        student.age = 32;
+        student.batchName = "Java Programming";
+        student.psp = 85.5;
+        student.email = "preeti.kumari@gmail.com";
+    }
+}
---
+++ src/main/java/org/example/introToOOPS/Student.java
@@ -0,0 +1,18 @@
+package main.java.org.example.introToOOPS;
+
+public class Student {
+    String name;
+    int age;
+    String batchName;
+    double psp;
+    String email;
+
+    void attendClass(){
+        System.out.println("Student is attending class.");
+    }
+
+    void bookMockInterview(){
+        System.out.println("Student is booking a mock interview.");
+    }
+
+}
