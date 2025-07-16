55f29aa8e4a3d9dd7430ec84db8a0d53943292f6
Preeti <preeti.kumari241116@gmail.com>
2025-07-16 13:29:57 +0530

    Fourth commit:16th July 2025: Manual/Parameterized Constructors

---
+++ src/main/java/org/example/introToOOPS4/ParameterizedConstructorOfStudent.java
@@ -0,0 +1,10 @@
+package main.java.org.example.introToOOPS4;
+
+public class ParameterizedConstructorOfStudent {
+    public static void main(String[] args) {
+        Student student = new Student("John Doe", 85.5);
+        System.out.println("DEBUG");
+    }
+
+
+}
---
+++ src/main/java/org/example/introToOOPS4/Student.java
@@ -0,0 +1,15 @@
+package main.java.org.example.introToOOPS4;
+
+public class Student {
+    String name ;
+    int age = 40 ;
+    double psp ;
+    String batchName ;
+    String email ;
+    boolean isEnrolled ;
+
+    Student(String name, Double psp){
+        name = name;
+        psp = psp;
+    }
+}
