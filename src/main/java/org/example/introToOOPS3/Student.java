622189d37caba454880ef9532249daa0e0e47a6c
Preeti <preeti.kumari241116@gmail.com>
2025-07-16 13:21:04 +0530

    Third commit:16th July 2025: Default Constructors

---
+++ src/main/java/org/example/introToOOPS3/Student.java
@@ -0,0 +1,8 @@
+package main.java.org.example.introToOOPS3;
+
+public class Student {
+    String Name;
+    int age = 21;
+    boolean isEnrolled = true;
+    String batchName = "Java Programming";
+}
---
+++ src/main/java/org/example/introToOOPS3/StudentsChildClass.java
@@ -0,0 +1,9 @@
+package main.java.org.example.introToOOPS3;
+
+public class StudentsChildClass{
+    public static void main(String[] args) {
+        Student student = new Student();
+        System.out.println("Name: " + student.Name);
+    }
+
+}
